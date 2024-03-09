e5dfe3e1e251c040a5d6718704069da6e825ea48
Aleksandr Fomichev <aleksandrfomicev260477@gmail.com>
2024-03-09 15:11:41 +0300

    Добавил два примера

--- task1/Program.cs
+++ task1/Program.cs
@@ -12,3 +12,12 @@ bool b = true;
 Console.WriteLine(d);
 Console.WriteLine(c);
 Console.WriteLine(b);
+
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
