b49af4e040b72d6e2989be0f0bf1709718c6dad8
Aleksandr Fomichev <aleksandrfomicev260477@gmail.com>
2024-03-09 14:18:49 +0300

    написал первую программу

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Всем привет");
 
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = 'i';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
